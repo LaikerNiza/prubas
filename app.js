4e248d3ffe38cef638fd445014780a31ecab39db
LaikerNiza <106284132+LaikerNiza@users.noreply.github.com>
2022-10-19 17:41:24 -0300

    si

---
+++ app.js
@@ -0,0 +1,8 @@
+import '.app.css';
+function app(){
+    return(
+        <div className="App Carlos">
+            <h1 className="text-center text-light pt-3">Hola Mundo</h1>
+        </div>
+    )
+}
