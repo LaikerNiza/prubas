ab5e1288d74c043113e72fc4800fff5383662cb8
LaikerNiza <106284132+LaikerNiza@users.noreply.github.com>
2022-10-19 17:53:33 -0300

    is

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-import '.app.css';
+import './app.css';
+
+function Usuarios() {
+    return (
+        <div className="bg-dark m-2 fondo">
+            <h3>Nombre: Julieta</h3>
+            <p>Edad: 25</p>
+
+        </div>
+    );
+
+}
+
+
+
+
+
+
+
+
+
 function app(){
-    return(
+    return (
         <div className="App Carlos">
             <h1 className="text-center text-light pt-3">Hola Mundo</h1>
         </div>
-    )
+);
 }
+
+export default app;
